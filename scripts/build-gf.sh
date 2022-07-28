42f7a5b3fec5d115113f2b4f81188195bdb088d6
Arthur HUGEAT <arthur.hugeat@jet1oeil.com>
2022-07-28 12:06:46 +0200

    Build dev and runtime version inside docker

--- scripts/build-gf.sh
+++ scripts/build-gf.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-RUNTIME_BUILD=1
+GIT_BRANCH=""
 CPACK_DEPENDENCIES=""
 CPACK_PACKAGE_SUFFIX=""
 
-while [ $# -gt 1 ]
+while [ $# -gt 0 ]
 do
 	case "$1" in
-	"--devel")
-		RUNTIME_BUILD=0
-		shift
+	"--branch")
+		if [ $# -ge 2 ]
+		then
+			GIT_BRANCH="$2"
+			shift 2
+		else
+			echo "Missing branch/tag name"
+			exit 1
+		fi
 		;;
-	"--dependencies")
-		if [ $# -gt 2 ]
+	"--runtime-dependencies")
+		if [ $# -ge 2 ]
 		then
 			CPACK_DEPENDENCIES="$2"
 			shift 2
 		else
-			echo "Missing dependencies list"
+			echo "Missing runtime dependencies list"
 			exit 1
 		fi
 		;;
 	"--package-suffix")
-		if [ $# -gt 2 ]
+		if [ $# -ge 2 ]
 		then
 			CPACK_PACKAGE_SUFFIX="$2"
 			shift 2
@@ -38,41 +44,38 @@ do
 	esac
 done
 
-if [ $# -ne 1 ]
+if [ -z "$GIT_BRANCH" ]
 then
 	echo "Missing branch or tag name"
 	exit 1
 fi
 
-if [ $RUNTIME_BUILD -eq 1 ] && [ -z "$CPACK_DEPENDENCIES" ]
+if [ -z "$CPACK_DEPENDENCIES" ]
 then
-	echo "No dependencies provided"
+	echo "No runtime dependencies provided"
 	exit 1
 fi
 
-if [ $RUNTIME_BUILD -eq 1 ] && [ -z "$CPACK_PACKAGE_SUFFIX" ]
+if [ -z "$CPACK_PACKAGE_SUFFIX" ]
 then
 	echo "Missing package suffix"
 	exit 1
 fi
 
-GIT_BRANCH="$1"
-
 git clone --depth 1 --branch "$GIT_BRANCH" --recursive https://github.com/GamedevFramework/gf.git
 
-rm -rf build/
+# Build runtime deb
+PACKAGE_NAME="gf-${GIT_BRANCH:1}${CPACK_PACKAGE_SUFFIX}"
+rm -rf build-runtime/
+cmake -DCMAKE_BUILD_TYPE=Release -DGF_BUILD_GAMES=OFF -DGF_BUILD_EXAMPLES=OFF -DGF_BUILD_DOCUMENTATION=OFF -DGF_SINGLE_COMPILTATION_UNIT=ON -DBUILD_TESTING=OFF -S gf -B build-runtime
+cmake --build build-runtime
+cpack --config build-runtime/CPackConfig.cmake -D CPACK_DEBIAN_PACKAGE_DEPENDS="$CPACK_DEPENDENCIES" -D CPACK_DEBIAN_PACKAGE_NAME="gf" -D CPACK_PACKAGE_FILE_NAME="$PACKAGE_NAME"
+cp "$PACKAGE_NAME.deb" packages/
 
-if [ $RUNTIME_BUILD -eq 1 ]
-then
-	PACKAGE_NAME="gf-${GIT_BRANCH:1}${CPACK_PACKAGE_SUFFIX}"
-	cmake -DCMAKE_BUILD_TYPE=Release -DGF_BUILD_GAMES=OFF -DGF_BUILD_EXAMPLES=OFF -DGF_BUILD_DOCUMENTATION=OFF -DGF_SINGLE_COMPILTATION_UNIT=ON -DBUILD_TESTING=OFF -S gf -B build
-	cmake --build build
-	cpack --config build/CPackConfig.cmake -D CPACK_DEBIAN_PACKAGE_DEPENDS="$CPACK_DEPENDENCIES" -D CPACK_DEBIAN_PACKAGE_NAME="gf" -D CPACK_PACKAGE_FILE_NAME="$PACKAGE_NAME"
-else
-	PACKAGE_NAME="gf-dev-${GIT_BRANCH:1}${CPACK_PACKAGE_SUFFIX}"
-	cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DGF_BUILD_GAMES=OFF -DGF_BUILD_EXAMPLES=OFF -DGF_BUILD_DOCUMENTATION=OFF -DGF_SINGLE_COMPILTATION_UNIT=ON -DBUILD_TESTING=OFF -S gf -B build
-	cmake --build build
-	cpack --config build/CPackConfig.cmake -D CPACK_PACKAGE_FILE_NAME="$PACKAGE_NAME"
-fi
-
+# Build dev deb
+PACKAGE_NAME="gf-dev-${GIT_BRANCH:1}${CPACK_PACKAGE_SUFFIX}"
+rm -rf build-dev/
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DGF_BUILD_GAMES=OFF -DGF_BUILD_EXAMPLES=OFF -DGF_BUILD_DOCUMENTATION=OFF -DGF_SINGLE_COMPILTATION_UNIT=ON -DBUILD_TESTING=OFF -S gf -B build-dev
+cmake --build build-dev
+cpack --config build-dev/CPackConfig.cmake -D CPACK_PACKAGE_FILE_NAME="$PACKAGE_NAME"
 cp "$PACKAGE_NAME.deb" packages/
