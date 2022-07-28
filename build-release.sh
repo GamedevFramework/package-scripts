838285b6a272ba37c8b841f2719b9f89a7dad667
Arthur HUGEAT <arthur.hugeat@jet1oeil.com>
2022-07-28 14:11:03 +0200

    Add script to automatize building

---
+++ build-release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Distributions
+declare -a DISTRIBUTIONS=("debian-bullseye")
+
+for DISTRIBUTION in ${DISTRIBUTIONS[@]}
+do
+  DISTRIBUTION_FAMILY=${DISTRIBUTION%%-*}
+  DISTRIBUTION_NAME=${DISTRIBUTION#*-}
+
+  # Build docker image
+  docker build -f "$DISTRIBUTION/Dockerfile" -t "gamedev-framework:$DISTRIBUTION" .
+
+  # Run compilation
+  docker run \
+    --user compile \
+    -v $PWD/packages:/home/compile/packages \
+    "gamedev-framework:$DISTRIBUTION" \
+    /bin/bash \
+      /home/compile/build-gf.sh \
+        --branch v1.0.0 \
+        --runtime-dependencies "libsdl2-2.0-0,libfreetype6,zlib1g,libpugixml1v5" \
+        --package-suffix "-${DISTRIBUTION_FAMILY}~${DISTRIBUTION_NAME}"
+done
